47ed10c38c44102502eeffce5c812aef7e035b71
vmc111 <vamship625@gmail.com>
2023-06-28 11:02:37 +0000

    5/6

--- Question1/question1.js
+++ Question1/question1.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -26,54 +27,39 @@ const startAndRunServer = async () => {
 
 startAndRunServer();
 
-// 1)1. Make a api for phone number login
-// a. Make add Customer api for customer, assume admin is adding customer ..
-// use the input params validation, code commenting, logging and check for
-// duplicates where required .
-
-// Assuming api url path to be "/register/" and post method is used to send phone number and userName
-
 app.post("/register/", async (request, response) => {
   const { number, username, password } = request.body;
   console.log(number);
-  //   Check if user is already registered
   const checkUserQuery = `SELECT * FROM user WHERE number = ${number};`;
   const userDetails = await db.get(checkUserQuery);
   if (userDetails !== undefined) {
     response.status(400);
-    response.send("Number already Registered");
+    response.send("Number already registered");
   } else {
-    // using Bcrypt to hash password before saving into Data Base
     const addUserQuery = `INSERT INTO user 
-        (
-            number, username, password
-        ) 
-        VALUES(
-            ${num}, '${username}', '${password}'
+        (number, username, password) 
+        VALUES (
+            ${number}, '${username}', '${password}'
         )`;
 
     await db.run(addUserQuery);
-    response.send("User added Successfully");
+    response.send("User added successfully");
   }
 });
 
-// Login Api assuming path to be "/login/"
-
 app.post("/login/", async (request, response) => {
   const { number, password } = request.body;
-  //validate input fields
   if (number === "" || password === "") {
     response.status(400);
-    response.send("number and password fields cannot be empty");
+    response.send("Number and password fields cannot be empty");
   } else {
-    const checkUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
-    //checking whether the user exists or not
-    const dbUser = await dataBase.get(checkUserQuery);
+    const checkUserQuery = `SELECT * FROM user WHERE number = '${number}';`;
+    const dbUser = await db.get(checkUserQuery);
     if (dbUser !== undefined) {
       const checkPassword = await bcrypt.compare(password, dbUser.password);
       if (checkPassword === true) {
         response.status(200);
-        response.send("Login Success");
+        response.send("Login success");
       } else {
         response.status(400);
         response.send("Invalid password");
--- Question3/question3.js
+++ Question3/question3.js
@@ -4,6 +4,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
+app.use(express.json());
 
 const dbPath = path.join(__dirname, "question3.db");
 
@@ -15,68 +16,71 @@ const initializeAndRunDb = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3000, () => {
-      console.log("SERVER RUNNING at http://localhost:3000");
-    });
+
+    // Move the app.listen() outside of the try-catch block
   } catch (error) {
     console.log(`DATA BASE ERROR: #${error.message}`);
     process.exit(1);
   }
 };
 
-initializeAndRunDb();
+initializeAndRunDb()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("SERVER RUNNING at http://localhost:3000");
+    });
+    const customers = [
+      {
+        email: "anurag11@yopmail.com",
+        name: "anurag",
+      },
+      {
+        email: "sameer11@yopmail.com",
+        name: "sameer",
+      },
+      {
+        email: "ravi11@yopmail.com",
+        name: "ravi",
+      },
+      {
+        email: "akash11@yopmail.com",
+        name: "akash",
+      },
+      {
+        email: "anjali11@yopmail.com",
+        name: "anjai",
+      },
+      {
+        email: "santosh11@yopmail.com",
+        name: "santosh",
+      },
+    ];
 
-const customers = [
-  {
-    email: "anurag11@yopmail.com",
-    name: "anurag",
-  },
-  {
-    email: "sameer11@yopmail.com",
-    name: "sameer",
-  },
-  {
-    email: "ravi11@yopmail.com",
-    name: "ravi",
-  },
-  {
-    email: "akash11@yopmail.com",
-    name: "akash",
-  },
-  {
-    email: "anjali11@yopmail.com",
-    name: "anjai",
-  },
-  {
-    email: "santosh11@yopmail.com",
-    name: "santosh",
-  },
-];
+    const insertData = async (customers) => {
+      for (const eachCustomer of customers) {
+        const { email, name } = eachCustomer;
 
-const insertData = async (customers) => {
-  customers.map(async (eachCustomer) => {
-    const { email, name } = eachCustomer;
-    // check if Email already exists
-    const checkEmailQuery = `SELECT * FROM customers WHERE email = '${email}'`;
-    const dbEmail = await dataBase.get(checkEmailQuery);
+        // check if Email already exists
+        const checkEmailQuery = `SELECT * FROM customers WHERE email = '${email}'`;
+        const dbEmail = await dataBase.get(checkEmailQuery);
 
-    if (dbEmail === undefined) {
-      const addQuery = `INSERT INTO customers 
-            (name, email)
-            VALUES (
-                '${name}', '${email}'
-            );`;
-      await dataBase.run(addQuery);
-    } else {
-      const updateNameQuery = `UPDATE customers
-            SET name = '${name}'
-            WHERE email = '${email}';`;
-    }
-    await dataBase.run(updateNameQuery);
+        if (dbEmail === undefined) {
+          const addQuery = `INSERT INTO customers (name, email) VALUES ('${name}', '${email}')`;
+          await dataBase.run(addQuery);
+        } else {
+          const updateNameQuery = `UPDATE customers SET name = '${name}' WHERE email = '${email}'`;
+          await dataBase.run(updateNameQuery);
+        }
+      }
+
+      const updatedTableQuery = `SELECT * FROM customers`;
+      const updatedTable = await dataBase.all(updatedTableQuery);
+      console.log(updatedTable);
+    };
+
+    insertData(customers);
+  })
+  .catch((error) => {
+    console.log(`ERROR: ${error.message}`);
+    process.exit(1);
   });
-  const updatedTableQuery = `SELECT * FROM customers`;
-  const updatedTable = await dataBase.all(updatedTableQuery);
-  console.log(updatedTable);
-};
-
-insertData(customers);
